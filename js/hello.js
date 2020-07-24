2ea5b80e5c76e9498216592b8484ec406e75cb87
duzit <duwei_oneday@163.com>
2020-07-24 22:55:51 +0800

    0724

--- js/hello.js
+++ js/hello.js
@@ -5,4 +5,23 @@ $(function() {
     height: '200px',
     background: 'red'
   })
+
+  $('.la').unbind();
+  $('.la').click(function() {
+    let ele = $(this).children('span');
+    ele.toggle(300)
+  })
+
+  $('.button1').click(function() {
+    $('.la').unbind();
+    $('.la').click(function() {
+      let ele = $(this).children('span');
+      ele.toggle(300)
+    })
+    $('.ul2').toggle();
+  })
+
+  $('.la').each(function(index) {
+    console.log(index, 0);
+  })
 })
